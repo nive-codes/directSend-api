8339acfa7b07cc1a4233bbcec8b19b7466490e22
chs <chs@sotongfive.kr>
2024-12-23 10:48:22 +0900

    first share

---
+++ src/main/java/com/api/directsend/DirectsendApplication.java
@@ -0,0 +1,13 @@
+package com.api.directsend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirectsendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DirectsendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/directsend/DirectsendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.directsend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DirectsendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
